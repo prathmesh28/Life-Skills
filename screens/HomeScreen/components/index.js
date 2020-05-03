84fcaf9818e051cb6d9c408c5d403def6e782d33
Prathmesh28 <prathmeshkerkar10@gmail.com>
2020-05-04 01:28:31 +0530

    updated

--- screens/HomeScreen/components/index.js
+++ screens/HomeScreen/components/index.js
@@ -17,7 +17,8 @@ const { width, height } = Dimensions.get("screen");
 import Firebase from "../../../firebase";
 import { Block } from "galio-framework";
 import { withNavigation } from "react-navigation";
-
+import _ from 'lodash'
+import { contains } from "react-native-redash";
 let userid;
 export default withNavigation(
   class Home extends React.Component {
@@ -29,14 +30,25 @@ export default withNavigation(
         searchQuery: '',
         hio: [],
         loading: false,
+        listloading: false, // user list loading
+        isRefreshing: false, 
       };
     }
-    onChangeSearch = query => {
-      this.setState({ searchQuery: query })
-      
+    onChangeSearch = text => {
+      // const formatQuery = text.toLowerCase()
+      this.setState({ searchQuery: text })
+    
     }
 
-
+    // shouldComponentUpdate () {
+    //   console.log("hi")
+    //   // const NewsData = this.state.NewsData.filter( item =>{
+    //   //   if(this.state.hio.includes(item.topic)){
+    //   //     return item
+    //   //   }
+    //   // })
+    //   // this.setState({NewsData})
+    // }
     componentDidMount() {
       this.setState({
         loading: true
@@ -58,6 +70,12 @@ export default withNavigation(
             }
           });
           this.setState({hio})
+          // const NewsData = this.state.NewsData.filter( item =>{
+          //   if(this.state.hio.includes(item.topic)){
+          //     return item
+          //   }
+          // })
+          // this.setState({NewsData})
           setTimeout(() => {
             this.setState({
               loading: false,
@@ -101,8 +119,99 @@ savelist = (props) => {
           );
         }
     })
+   
   
 }
+// renderItem = ({ item }) => (<Vi
+renderItem = ({item}) => {
+  if(this.state.hio.includes(item.topic)){
+    return( 
+      <Card  style={styles.card}>
+        <Card.Title
+          key={item.id}
+          title={item.topic}
+          titleStyle={styles.titlecard}
+          right={(props) => 
+            <ToggleButton
+                icon="heart"
+                color={Colors.pink300}
+                //  status={item.Saved}
+                onPress={ () => this.savelist(item)}
+              ></ToggleButton>
+          }
+          rightStyle={styles.righticon}
+          style={styles.cardsty}
+        />
+        {/* <RNUrlPreview  
+          text={item.link} 
+          titleStyle={styles.linktitle}
+          containerStyle={styles.linkcontainer}
+          titleNumberOfLines={2}
+          imageStyle={styles.linkimage}
+          disabled
+          /> */}
+        
+        <Card.Content style={styles.styleurl}>
+          <ActivityIndicator style={styles.loadcards} size="large" color="#741cc7"/> 
+       
+          <RNUrlPreview  
+            text={item.link} 
+            titleStyle={styles.linktitle}
+            containerStyle={styles.linkcontainer}
+            titleNumberOfLines={3}
+            imageStyle={styles.imagesty}
+            descriptionStyle ={styles.linkimage}
+          />
+          <RNUrlPreview  
+            text={item.link} 
+            title={false}
+            containerStyle={styles.linkcontainer}
+            imageStyle={styles.linkimage}
+            descriptionStyle ={styles.discript}
+            descriptionNumberOfLines={4}
+          />
+        </Card.Content>
+
+      </Card>
+    )}
+}
+
+onRefresh() {
+  this.setState({isRefreshing:true})
+  Firebase.database()
+        .ref("/topiclist/")
+        .once("value", (snapshot) => {
+          this.setState({NewsData:snapshot.val()})
+     
+
+      var currentIndex = this.state.NewsData.length, temporaryValue, randomIndex;
+      console.log(currentIndex)
+      let array = this.state.NewsData
+      while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+    
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+      this.setState({NewsData:array})
+    })
+      this.setState({isRefreshing:false})
+
+}
+renderFooter = () => {
+  //it will show indicator at the bottom of the list when data is loading otherwise it returns null
+  //  if (!this.state.listloading) return null;
+   return (
+     <ActivityIndicator
+       style={{ color: '#000' }}
+     />
+   );
+ };
 
   render() {
     const { searchQuery } = this.state;
@@ -120,60 +229,18 @@ savelist = (props) => {
         <FlatList
           data={this.state.NewsData}
           keyExtractor={item => item.id.toString()}
-          showsVerticalScrollIndicator={false}
-        // onRefresh={() => }      
-          renderItem={({ item }) => { 
-            if(this.state.hio.includes(item.topic)){
-            return( 
-              <Card  style={styles.card}>
-                <Card.Title
-                  key={item.id}
-                  title={item.topic}
-                  titleStyle={styles.titlecard}
-                  right={(props) => 
-                    <ToggleButton
-                        icon="heart"
-                        color={Colors.pink300}
-                        //  status={item.Saved}
-                        onPress={ () => this.savelist(item)}
-                      ></ToggleButton>
-                  }
-                  rightStyle={styles.righticon}
-                  style={styles.cardsty}
-                />
-                {/* <RNUrlPreview  
-                  text={item.link} 
-                  titleStyle={styles.linktitle}
-                  containerStyle={styles.linkcontainer}
-                  titleNumberOfLines={2}
-                  imageStyle={styles.linkimage}
-                  disabled
-                  /> */}
-                
-                <Card.Content style={styles.styleurl}>
-                  <ActivityIndicator style={styles.loadcards} size="large" color="#741cc7"/> 
-
-                  <RNUrlPreview  
-                    text={item.link} 
-                    titleStyle={styles.linktitle}
-                    containerStyle={styles.linkcontainer}
-                    titleNumberOfLines={3}
-                    imageStyle={styles.imagesty}
-                    descriptionStyle ={styles.linkimage}
-                  />
-                  <RNUrlPreview  
-                    text={item.link} 
-                    title={false}
-                    containerStyle={styles.linkcontainer}
-                    imageStyle={styles.linkimage}
-                    descriptionStyle ={styles.discript}
-                    descriptionNumberOfLines={4}
-                  />
-                </Card.Content>
-
-              </Card>
-            )}
-          }}
+          // showsVerticalScrollIndicator={false}
+          initialNumToRender={10}
+          refreshing={this.state.isRefreshing}
+          onRefresh={this.onRefresh.bind(this)}
+          getItemLayout={(data, index) => (
+            {length: height*0.24, offset: height*0.24 * index, index}
+          )}     
+          ListFooterComponent={this.renderFooter.bind(this)}
+          renderItem={this.renderItem}  
+          onEndReachedThreshold={0.4}
+          // onEndReached={this.handleLoadMore.bind(this)}
+        
         />
       </View>
     )     
